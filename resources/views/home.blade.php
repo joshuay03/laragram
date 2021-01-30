ae100b63a85ce08a12d2eefc317f99f329ef641d
Joshua Young <djry1999@gmail.com>
2021-01-30 23:42:52 +1000

    Replicate profile page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5" >
+            <img src="https://scontent-syd2-1.cdninstagram.com/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=scontent-syd2-1.cdninstagram.com&_nc_ohc=Qs0j1hhrQ-IAX_8vGqf&tp=1&oh=173771a0c1049c9564118a883026ce38&oe=603FF9E7" class="rounded-circle" alt="">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>Testing</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>123 </strong>posts</div>
+                <div class="pr-5"><strong>123k </strong>followers</div>
+                <div class="pr-5"><strong>123 </strong>following</div>
             </div>
+            <div class="pt-4 font-weight-bold">testing.org</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+            <div><a href="#">www.testing.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://scontent-syd2-1.cdninstagram.com/v/t51.2885-15/e35/c2.0.824.824a/s320x320/143377875_747593889489606_5323934135154740495_n.jpg?_nc_ht=scontent-syd2-1.cdninstagram.com&_nc_cat=106&_nc_ohc=4dEPFR913mEAX8PrrK0&tp=1&oh=6f892167e48e470e60681def9f87d906&oe=603F0DAE" alt="">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-syd2-1.cdninstagram.com/v/t51.2885-15/e35/c2.0.824.824a/s320x320/143377875_747593889489606_5323934135154740495_n.jpg?_nc_ht=scontent-syd2-1.cdninstagram.com&_nc_cat=106&_nc_ohc=4dEPFR913mEAX8PrrK0&tp=1&oh=6f892167e48e470e60681def9f87d906&oe=603F0DAE" alt="">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-syd2-1.cdninstagram.com/v/t51.2885-15/e35/c2.0.824.824a/s320x320/143377875_747593889489606_5323934135154740495_n.jpg?_nc_ht=scontent-syd2-1.cdninstagram.com&_nc_cat=106&_nc_ohc=4dEPFR913mEAX8PrrK0&tp=1&oh=6f892167e48e470e60681def9f87d906&oe=603F0DAE" alt="">
         </div>
     </div>
 </div>
