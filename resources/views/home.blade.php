b38f4e7925739fa3ebe04fc3459e06853170f885
Joshua Young <djry1999@gmail.com>
2021-02-02 15:25:12 +1000

    Introduce user profiles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,15 @@
             <img src="https://scontent-syd2-1.cdninstagram.com/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=scontent-syd2-1.cdninstagram.com&_nc_ohc=Qs0j1hhrQ-IAX_8vGqf&tp=1&oh=173771a0c1049c9564118a883026ce38&oe=603FF9E7" class="rounded-circle" alt="">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>Testing</h1></div>
+            <div><h1>{{ $user->username }}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>123 </strong>posts</div>
                 <div class="pr-5"><strong>123k </strong>followers</div>
                 <div class="pr-5"><strong>123 </strong>following</div>
             </div>
-            <div class="pt-4 font-weight-bold">testing.org</div>
-            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
-            <div><a href="#">www.testing.org</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url ?? 'N/A'}}</a></div>
         </div>
     </div>
     <div class="row pt-5">
