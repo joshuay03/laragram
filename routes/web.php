251fb57e0b32b9adc8303536e55b408d9bdb3c95
Joshua Young <djry1999@gmail.com>
2021-02-05 20:23:16 +1000

    Configure editing of user profile

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
+Route::get('/profile/{user}/edit', [App\Http\Controllers\ProfilesController::class, 'edit'])->name('profile.edit');
+Route::patch('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'update'])->name('profile.update');
 
 Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);
 Route::post('/p', [App\Http\Controllers\PostsController::class, 'store']);
-
 Route::get('/p/{post}', [App\Http\Controllers\PostsController::class, 'show']);
