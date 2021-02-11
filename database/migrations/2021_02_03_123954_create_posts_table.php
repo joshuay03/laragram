fe44fd15d72c5ffca8173f347b5230f7b189040d
Joshua Young <djry1999@gmail.com>
2021-02-11 20:59:48 +1000

    Install telescope and introduce posts index

--- database/migrations/2021_02_03_123954_create_posts_table.php
+++ database/migrations/2021_02_03_123954_create_posts_table.php
@@ -15,12 +15,11 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->index('user_id');
             $table->unsignedBigInteger('user_id');
             $table->string('caption');
             $table->string('image');
             $table->timestamps();
-
-            $table->index('user_id');
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
+
+Route::get('/', [App\Http\Controllers\PostsController::class, 'index']);
 
 Route::get('/profile/{user}/edit', [App\Http\Controllers\ProfilesController::class, 'edit'])->name('profile.edit');
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
